98817f918c1fba081b546a5070a07c20153f6720
Pavel_Sorokin <sorokin.zxcv@gmail.com>
2024-11-25 12:46:18 +0400

    ip for server

--- app/src/main/java/com/example/flagmanstorage/API/ApiClient.kt
+++ app/src/main/java/com/example/flagmanstorage/API/ApiClient.kt
@@ -3,7 +3,7 @@ package com.example.flagmanstorage.API
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 object ApiClient {
-    private const val BASE_URL = "http://192.168.89.52:8080"  // URL вашего сервера
+    private const val BASE_URL = "http://10.0.0.250:8080"    // URL вашего сервера
 
     private var retrofit: Retrofit? = null
 
